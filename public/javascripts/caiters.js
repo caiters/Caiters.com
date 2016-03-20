f158a3a8aded5cd6998f45406471869ddf21ea61
Caitlin <ayashi@valefor.com>
2016-03-20 14:58:36 -0700

    adding in some generic page banners, styling project list, adding class when page is scrolled

--- public/javascripts/caiters.js
+++ public/javascripts/caiters.js
@@ -1,3 +1,13 @@
 (function(){
   // Desktop/Tablet only: add class to body when you are no longer at the top of the page, so we can slide over the name in the nav.
+  var scrollTop;
+  $(document).scroll(function(e){
+    scrollTop = $(window).scrollTop();
+    console.log(scrollTop);
+    if ( scrollTop > 0) {
+      $('body').addClass('is-scrolled');
+    } else {
+      $('body').removeClass('is-scrolled');
+    }
+  });
 })();
