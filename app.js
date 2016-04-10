11786157560d5d55f8f2e62ac72ab0220d6d49b6
Caitlin <ayashi@valefor.com>
2016-04-09 22:22:12 -0700

    getting the contact form to work

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var moment = require('moment');
+var nodemailer = require('nodemailer');
 
 var routes = require('./routes/index');
 
@@ -23,6 +24,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var gmailpw = process.env.CAITERS_GMAIL_PASS;
+var transporter = nodemailer.createTransport('smtps://ayashi%40valefor.com:'+ gmailpw +'@smtp.gmail.com');
+
+app.post('/contact', function(req,res){
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;', // grab form data from the request body object
+    to: 'ayashi+caiters@valefor.com',
+    subject: 'Caiters.com contact form',
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOpts, function(error, response){
+    if (error) {
+      // Email not sent
+      res.json({ msg: 'Error occured - message not sent', err:true });
+    } else {
+      // Email sent!
+      res.json({ msg: 'Message sent! Thank you.', err:false });
+    }
+  });
+});
 
 app.use('/', routes);
 
--- public/javascripts/caiters.js
+++ public/javascripts/caiters.js
@@ -9,4 +9,90 @@
       $('body').removeClass('is-scrolled');
     }
   });
+
+  /*
+  Contact form to do-
+  - prevent clicking when btn--submitting class is on the button
+  - show visual for submitting button so user isn't confused why nothing is happening (ajax loader)
+  - display "message sent" or "message not sent" messages if the form succeeds/fails
+  */
+
+  // contact form
+  if( $('#contactForm').length>0){
+
+    // submit form
+    $('#contactForm').submit(function(e){
+      e.preventDefault();
+      if(validateForm()){
+        // it's good!
+        $('#sendmsg').addClass('btn--submitting');
+        var formData = {
+          'name': $('input[name=name]').val(),
+          'email': $('input[name=email]').val(),
+          'message': $('textarea[name=message]').val(),
+        };
+
+        $.ajax({
+          type: 'POST',
+          url: '/contact',
+          data: formData,
+          dataType: 'json',
+          encode: true
+        }).done(function(data){
+          $('#sendmsg').removeClass('btn--submitting');
+          console.log(data);
+        });
+
+
+      } else {
+        // it didn't validate
+        console.error('error');
+      }
+    });
+
+    // form validation
+    var validateForm = function(){
+      var error = false;
+      if(!($('#name').val().length > 0)) {
+        // if name length is 0, error
+        $('#name').addClass('form-error');
+        $('#name').next('.form-error-msg').text('Please enter your name').removeClass('form-error-msg--hidden');
+        error = true;
+      }
+      if ( $('#email').val().length > 0){
+        var re = /\S+@\S+/;
+        // check if email is a valid email address string@string
+        if (!re.test($('#email').val())){
+          $('#email').addClass('form-error');
+          $('#email').next('.form-error-msg').text('Please enter a valid email address').removeClass('form-error-msg--hidden');
+          error = true;
+        }
+      } else {
+        // if email length is 0, error
+        $('#email').addClass('form-error');
+        $('#email').next('.form-error-msg').text('Please enter an email address').removeClass('form-error-msg--hidden');
+        error = true;
+      }
+      if( $('#message').val().length === 0) {
+        // if message length is 0, error
+        $('#message').addClass('form-error');
+        $('#message').next('.form-error-msg').text('Please write a message').removeClass('form-error-msg--hidden');
+        error = true;
+      }
+      // if we have any errors, not valid.
+      if(error){
+        return false;
+      } else { return true; }
+    };
+
+    $('form input, form textarea').keyup(function(){
+      var $this = $(this);
+      if( $this.hasClass('form-error')){
+        $this.removeClass('form-error');
+        var errorMsg = $this.next('.form-error-msg')[0];
+        console.log(errorMsg);
+        $(errorMsg).addClass('form-error-msg--hidden');
+      }
+    });
+  };
 })();
